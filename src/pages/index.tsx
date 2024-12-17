573955e778054287e9a74d23ad9bd75f3209f27b
Ender Spiriit <rico.dharmawan198@gmail.com>
2024-12-17 16:28:06 +0700

    more homepage update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,14 @@ export default function Home() {
     router.push('/login');
   }
 
+  
+  // window.addEventListener("load", () => {
+  //   for (let i=0; i < 4; i++) {
+  //     const element = document.getElementById('home_transition'+i)
+  //     element?.classList.add("-translate-y-5");
+  //   }
+  // });
+  
   return (
     <div className="h-screen font-[family-name:var(--font-geist-sans)]">
       <div id="bg"></div>
@@ -20,7 +28,7 @@ export default function Home() {
         </div>
       </div>
       <div className="absolute flex inset-0 top-1/3 left-[10%]">
-        <div className="mx-5 h-72 w-[45rem] text-wrap pt-40">
+        <div id="scroll" className="mx-5 h-72 w-[45rem] text-wrap mt-36">
           <p data-name="animate" className="text-8xl font-bold text-white mb-8">Cardano SafeTx</p>
           <p data-name="animate" className="text-lg text-white mb-10 pl-2">
             Cardano SafeTx is a brand new website to make Cardano cryptocurrency transactions.
@@ -29,16 +37,16 @@ export default function Home() {
             as connecting your dApp wallet with a browser extension!
           </p>
         </div>
-        <div className="mx-5 h-[32rem] w-[32rem] text-wrap">
+        <div id="scroll" className="mx-5 h-[32rem] w-[32rem] text-wrap">
           <Image id="img" src="/cardano.webp" alt="cardano" width={512} height={512} className="relative rounded-full border-8 inset-0 left-52" />
         </div>
       </div>
       <div className="absolute flex inset-0 top-[70rem] left-[10%]">
-        <div className="mx-5 h-[32rem] w-[40rem] text-wrap">
-          <Image id="anim-img" src="/eternl.png" alt="cardano" width={400} height={400} className="absolute rotate-[-25deg] inset-0 -left-10 bg-white rounded-full z-10" />
-          <Image id="anim-img" src="/lace.jpg" alt="cardano" width={300} height={300} className="absolute inset-0 left-64 top-16 rotate-[15deg] bg-white rounded-full" />
+        <div id="scroll" className="mx-5 h-[32rem] w-[40rem] mr-28">
+          <Image id="anim-img" src="/eternl.png" alt="cardano" width={400} height={400} className="absolute inset-0 -left-10 bg-white rounded-full z-10" />
+          <Image id="anim-img1" src="/lace.jpg" alt="cardano" width={300} height={300} className="absolute inset-0 left-64 top-16 bg-white rounded-full" />
         </div>
-        <div className="mx-5 h-72 w-[45rem] text-wrap pt-24">
+        <div id="scroll" className="mx-5 h-72 w-[45rem] text-wrap mt-28">
           <p data-name="animate" className="text-8xl font-bold text-white mb-8">What is dApp?</p>
           <p data-name="animate" className="text-lg text-white mb-10 pl-2">
             A decentralized application (dApp) is a software application that runs on a blockchain or a distributed network of computers rather than relying on a single centralized server. These applications leverage the features of decentralization, transparency, and security provided by the underlying blockchain or distributed ledger technology.
